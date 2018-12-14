c2472765c5f4f4c1ea3c39f368cf94a98a2d85f8
Daniel Oaks <doaks@londontrustmedia.com>
2018-12-14 11:28:31 +1000

    Use new gems for building, start using plugins to add file update times and such

--- Gemfile
+++ Gemfile
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 source "https://rubygems.org"
 
-gem 'github-pages', group: :jekyll_plugins
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+gem "jekyll", "~> 3.8"
+
+gem "octopress-hooks", "~> 2.6"
+
+gem "git", "~> 1.5"
---
+++ _plugins/irchooks.rb
@@ -0,0 +1,32 @@
+require 'octopress-hooks'
+require 'git'
+
+module Jekyll
+    class IRCPage < Octopress::Hooks::Page
+
+        attr_accessor :g
+
+        def pre_render(page)
+            # set raw markdown data
+            page.data["rawmd"] = page.content
+            page.data["rawmd"].gsub! "{% include numeric-format-header.html %}", ""
+
+            # set file commit date/time
+            if page.site.config["pregit"] then
+                if @g == nil then
+                    @g = Git.open(Dir.pwd)
+                end
+                page_file = File.join(".", page.dir, page.name)
+                commit = @g.log().object(page_file).first
+                page.data["commit-id"] = "fake"
+                earliest_date = Time.now
+                if commit then
+                    earliest_date = commit.date
+                    page.data["commit-id"] = commit.sha
+                end
+                page.data["commit-date"] = earliest_date
+            end
+        end
+
+      end
+end
